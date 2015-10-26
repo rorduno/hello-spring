0f1800b65238ae55d701f2d3b467b7ed2793d2b2
Raul Orduno <rorduno88@gmail.com>
2015-10-26 14:20:30 -0500

    updated

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 	MessageService mockMessageService() {
 		return new MessageService() {
 			public String getMessage() {
-				return "Hello Spring!";
+				return "Hello World!";
 			}
 		};
 	}
